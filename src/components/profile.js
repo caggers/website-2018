d9afc1ba7a6a246be3bde93ad1c062f1c7e3f55a
Gemma Cagney <gemmacagney@gmail.com>
2018-08-17 16:30:02 +0100

    experience section added

--- src/components/profile.js
+++ src/components/profile.js
@@ -3,7 +3,6 @@ import styled from 'styled-components';
 import PropTypes from 'prop-types';
 
 import profileImg from '../images/profile-pic.png';
-import coderDojo from '../images/coder-dojo.jpg';
 import SectionHeader from './section/sectionHeader';
 import SectionContent from './section/sectionContent';
 
@@ -40,12 +39,6 @@ const ExtraContent = styled.div`
   padding-top: 15px;
 `
 
-const CoderDojoImg = styled.img`
-  width: 75%;
-  margin: 5% 10%
-`
-
-
 export default class Profile extends React.Component {
 
   render() {
@@ -61,15 +54,6 @@ export default class Profile extends React.Component {
         </ProfileTitle>
         <ProfileContent>
           <SectionContent content={text} />
-          <ExtraContent>
-            I am passionate about technology and reaching out to the next generation of women in Tech.
-            I’ve written some blogs about my experiences so far which you can read
-            <a href={blogs[0]}> here </a>,
-            <a href={blogs[1]}> here </a>, and
-            <a href={blogs[2]}> here </a>.
-            I am really proud of my work with Coder Dojo; two ninjas I mentored went on to win European Digital Girl of the year in 2017 and in May 2018 I spoke on a panel at Coolest Projects.
-            {/* <CoderDojoImg src={coderDojo} alt="Me on stage" /> */}
-          </ExtraContent>
         </ProfileContent>
       </ProfileWrapper>
     );
--- src/components/section/sectionContent.js
+++ src/components/section/sectionContent.js
@@ -4,6 +4,7 @@ import styled from 'styled-components';
 
 const MainText = styled.div`
   font-family: Josefin Slab, Arial, Verdana, sans-serif;
+  padding: 10px 0;
 `
 
 const SectionContent = ({content}) => (
